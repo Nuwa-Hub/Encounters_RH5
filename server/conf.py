a4e5b57ca13d3ce60ce4b686f2e61506695cb581
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-21 04:10:01 +0530

    Added a Kafka Listner to get user_id and meeting_id and save in db

--- server/conf.py
+++ server/conf.py
@@ -1,8 +1,5 @@
 import os
 
-mongo_db_host = os.getenv("mongo_db_host")
-mongo_db_port = int(os.getenv("mongo_db_port"))
-mongo_db_user = os.getenv("mongo_db_user")
-mongo_db_password = os.getenv("mongo_db_password")
-mongo_db_database = os.getenv("mongo_db_database")
-mongo_db_auth = os.getenv("mongo_db_auth")
+mongod_db_connection_string = os.getenv("mongo_db_connection_string")
+
+NOTIFICATIONS_TOPIC = "notifications"
--- server/models/__init__.py
+++ server/models/__init__.py
@@ -3,14 +3,6 @@ import motor.motor_asyncio
 
 from server import conf
 
-async_client = MongoClient(conf.mongo_db_host,
-                     port=conf.mongo_db_port,
-                     username=conf.mongo_db_user,
-                     password=conf.mongo_db_password,
-                     authSource=conf.mongo_db_auth)
+async_client = MongoClient(conf.mongod_db_connection_string)
 
-client = motor.motor_asyncio.AsyncIOMotorClient(conf.mongo_db_host,
-                     port=conf.mongo_db_port,
-                     username=conf.mongo_db_user,
-                     password=conf.mongo_db_password,
-                     authSource=conf.mongo_db_auth)
+client = motor.motor_asyncio.AsyncIOMotorClient(conf.mongod_db_connection_string)
