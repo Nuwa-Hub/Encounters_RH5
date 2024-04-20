87c04b74260e8e9f076521f6cf8797ab349fbd75
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-20 20:02:32 +0530

    Added models folder

---
+++ server/conf.py
@@ -0,0 +1,8 @@
+import os
+
+mongo_db_host = os.getenv("mongo_db_host")
+mongo_db_port = int(os.getenv("mongo_db_port"))
+mongo_db_user = os.getenv("mongo_db_user")
+mongo_db_password = os.getenv("mongo_db_password")
+mongo_db_database = os.getenv("mongo_db_database")
+mongo_db_auth = os.getenv("mongo_db_auth")
