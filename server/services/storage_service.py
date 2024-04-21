62a8dd12cf8c0365d9ef36dcd30766235762faba
deshapriya <deshapriya@orangehrm.com>
2024-04-21 09:28:35 +0530

    add

--- server/services/storage_service.py
+++ server/services/storage_service.py
@@ -1,35 +1,15 @@
-#want get data with boto3
-# Path: services/storage_service.py
-import boto3
 import os
-import io
-import numpy as np
-from numpy.linalg import norm
+import tempfile
+import requests
 
-from server.services import inference
-
-def download_wav(original_file, interview_file):
-    s3 = boto3.resource(
-        service_name='s3',
-        region_name=os.getenv('AWS_REGION'),
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS KEY')
-    )
-                                        
-    bucket = os.getenv('S3_BUCKET')
-    s3.download_file(bucket, original_file, 'original.wav')
-    s3.download_file(bucket, interview_file, 'interview.wav')
-
-# Path: services/storage_service.py
-
-def download_img(original_file, interview_file):
-    s3 = boto3.resource(
-        service_name='s3',
-        region_name=os.getenv('AWS_REGION'),
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS KEY')
-    )
-
-    bucket = os.getenv('S3_BUCKET')
-    s3.download_file(bucket, original_file, 'original.png')
-    s3.download_file(bucket, interview_file, 'interview.png')
+async def download_image(image_url: str):
+    
+    response = requests.get(image_url, stream=True)
+    if response.status_code == 200:
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as f:
+            for chunk in response.iter_content(chunk_size=128):
+                f.write(chunk)
+        return f.name
+    else:
+        return None
